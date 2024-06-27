c01234d1ced9015546d880f3e20e93d4c6ee622b
Samuel Lucas <samuellucas188@gmail.com>
2024-06-27 18:43:12 -0300

    Novo commit

--- project_recipes/urls.py
+++ project_recipes/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('recipes.urls')),
+    path('', include('recipes.urls')),
   
 ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from recipes.views import home
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('',home),
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),    
     
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
